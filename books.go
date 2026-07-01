3a2a2d8ce6e45c970d82527383a5aec76a61026d
tladmin@tltestlab.com <tladmin@tltestlab.com>
2026-07-01 16:20:02 +0100

    feat: create books package

---
+++ books.go
@@ -0,0 +1,13 @@
+package books
+
+import "fmt"
+
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+}
+
+func BookToString(book Book) string {
+	return fmt.Sprintf("%v by %v (copies: %v)", book.Title, book.Author, book.Copies)
+}
