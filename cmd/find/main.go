f8df63a1a72e77b98146f5b4a95a651ae4515bae
tladmin@tltestlab.com <tladmin@tltestlab.com>
2026-07-03 15:26:49 +0100

    refactor: update find with args

--- cmd/find/main.go
+++ cmd/find/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"books"
 	"fmt"
+	"os"
 )
 
 func main() {
-	book, ok := books.GetBook("xyz")
+	ID := os.Args[1]
+	book, ok := books.GetBook(ID)
 	if !ok {
 		fmt.Println("Sorry, I couldn't find that book in the catalog.")
 		return
